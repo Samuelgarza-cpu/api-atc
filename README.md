d58ca8062c785952f9e71ec128d845123a2797fa
Samuelgarza-cpu <78004493+Samuelgarza-cpu@users.noreply.github.com>
2023-10-21 20:00:09 -0600

    PRIMER VERSION ESTABLE

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Proyecto Escolar
 
+php artisan migrate:fresh --path=database/migrations/GomezApp --database=mysql_gomezapp
+php artisan db:seed --class=DatabaseGomezAppSeeder
+
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+
+
+#region CONTROLLERS INGRESOS
+
+
+#endregion CONTROLLERS INGRESOS
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Route::api('usuario',[UsuarioController::class,'index']);
+
+
+
+Route::prefix('gomezapp')->group(function () {
+    Route::get('/', function () {
+        return 'API GomezApp';
+    });
+    include_once "gomezapp.routes.php";
 });
