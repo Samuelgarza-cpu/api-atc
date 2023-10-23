548794680398ce9a3e8bf2ca7cecb48e5f4b9b8c
PSONTE <101921010+PSONTE@users.noreply.github.com>
2023-10-23 15:14:25 -0600

    VISTA ASUNTOS DEP

---
+++ database/migrations/2023_09_03_154826_create_asuntos_dep_view.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+        CREATE 
+        VIEW `asuntos_dep` AS
+            SELECT 
+                `departamentos_asuntos`.`department_id` AS `department_id`,
+                `departamentos_asuntos`.`asunto_id` AS `asunto_id`,
+                `asuntos`.`asunto` AS `asunto`
+            FROM
+                (`departamentos_asuntos`
+                JOIN `asuntos` ON ((`asuntos`.`id` = `departamentos_asuntos`.`asunto_id`)))
+    ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW IF EXISTS asuntos_dep');
+    }
+};
