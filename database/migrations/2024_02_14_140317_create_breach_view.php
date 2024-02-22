e8f70980aeb9d591cd147b915870e7493cfd6b71
SAMUEL G <samuelgarza1029@gmail.com>
2024-02-22 10:13:46 -0600

    SE MODIFICA MIGRACION DE INCUMPLIMIENTO SIN FINES DE SEMANA

--- database/migrations/2024_02_14_140317_create_breach_view.php
+++ database/migrations/2024_02_14_140317_create_breach_view.php
@@ -17,36 +17,65 @@ return new class extends Migration
         DB::statement("
         CREATE VIEW incumplimiento as
         SELECT 
-        `sp_requests`.`id` AS `id`,
-        `sp_requests`.`folio` AS `folio`,
-        `sp_requests`.`nombre` AS `nombre`,
-        `sp_requests`.`app` AS `app`,
-        `sp_requests`.`apm` AS `apm`,
-        `sp_requests`.`tipo_documento` AS `tipo_documento`,
-        `sp_requests`.`observaciones` AS `observaciones`,
-		`sp_requests`.`created_at` AS `created_at`,
-        `sp_requests`.`visto_at` AS `visto_at`,
-		`sp_requests`.`respuesta_at` AS `respuesta_at`,
-        `sp_requests`.`respuesta` AS `respuesta`,
-		`sp_requests`.`completado_at` AS `completado_at`,
-        `sp_requests`.`estatus` AS `estatus`,
-        `departments`.`department` AS `department`,
-        `asuntos`.`asunto` AS `asunto`,
-        datediff(`sp_requests`.`completado_at`,`sp_requests`.`created_at`) AS `dias_transcurridos`,
-        (TO_DAYS(NOW()) - TO_DAYS(`sp_requests`.`created_at`))
-    FROM
-        ((`sp_requests`
-        JOIN `departments` ON ((`departments`.`id` = `sp_requests`.`id_departamento_destino`)))
-        JOIN `asuntos` ON ((`sp_requests`.`id_asunto` = `asuntos`.`id`)))
-    WHERE
-        ((`sp_requests`.`active` = 1)
-			AND (datediff(`sp_requests`.`completado_at`,`sp_requests`.`created_at`) > 5 ) OR (TO_DAYS(NOW()) - TO_DAYS(`sp_requests`.`created_at`) > 5)
-            AND ((`sp_requests`.`estatus` = 'ALTA')
-            OR (`sp_requests`.`estatus` = 'COMPLETA'))
-			AND(isnull(`sp_requests`.`completado_at`) OR (`sp_requests`.`completado_at` > `sp_requests`.`created_at`) ) 
-            AND `sp_requests`.`active` = 1
-            )
-         
+    `sp_requests`.`id` AS `id`,
+    `sp_requests`.`folio` AS `folio`,
+    `sp_requests`.`nombre` AS `nombre`,
+    `sp_requests`.`app` AS `app`,
+    `sp_requests`.`apm` AS `apm`,
+    `sp_requests`.`tipo_documento` AS `tipo_documento`,
+    `sp_requests`.`observaciones` AS `observaciones`,
+    `sp_requests`.`created_at` AS `created_at`,
+    `sp_requests`.`visto_at` AS `visto_at`,
+    `sp_requests`.`respuesta_at` AS `respuesta_at`,
+    `sp_requests`.`respuesta` AS `respuesta`,
+    `sp_requests`.`completado_at` AS `completado_at`,
+    `sp_requests`.`estatus` AS `estatus`,
+    `departments`.`department` AS `department`,
+    `asuntos`.`asunto` AS `asunto`,
+    DATEDIFF(NOW(), `sp_requests`.`created_at`) - (WEEK(NOW()) - WEEK(`sp_requests`.`created_at`)) * 2 - (CASE
+        WHEN WEEKDAY(`sp_requests`.`created_at`) = 6 THEN 1
+        ELSE 0
+    END) - (CASE
+        WHEN WEEKDAY(NOW()) = 5 THEN 1
+        ELSE 0
+    END) AS dias_transcurridos,
+        DATEDIFF(`sp_requests`.`completado_at`, `sp_requests`.`created_at`) - (WEEK(`sp_requests`.`completado_at`) - WEEK(`sp_requests`.`created_at`)) * 2 - (CASE
+        WHEN WEEKDAY(`sp_requests`.`created_at`) = 6 THEN 1
+        ELSE 0
+    END) - (CASE
+        WHEN WEEKDAY(`sp_requests`.`completado_at`) = 5 THEN 1
+        ELSE 0
+    END) AS dias_transcurridos_con_completar
+FROM
+    ((`sp_requests`
+    JOIN `departments` ON ((`departments`.`id` = `sp_requests`.`id_departamento_destino`)))
+    JOIN `asuntos` ON ((`sp_requests`.`id_asunto` = `asuntos`.`id`)))
+WHERE
+        (
+         ((`sp_requests`.`estatus` = 'ALTA')
+        OR (`sp_requests`.`estatus` = 'COMPLETA'))
+    AND ( DATEDIFF(`sp_requests`.`completado_at`, `sp_requests`.`created_at`) - (WEEK(`sp_requests`.`completado_at`) - WEEK(`sp_requests`.`created_at`)) * 2 - (CASE
+        WHEN WEEKDAY(`sp_requests`.`created_at`) = 6 THEN 1
+        ELSE 0
+    END) - (CASE
+        WHEN WEEKDAY(`sp_requests`.`completado_at`) = 5 THEN 1
+        ELSE 0
+    END) > 5) OR (ISNULL(DATEDIFF(`sp_requests`.`completado_at`, `sp_requests`.`created_at`) - (WEEK(`sp_requests`.`completado_at`) - WEEK(`sp_requests`.`created_at`)) * 2 - (CASE
+        WHEN WEEKDAY(`sp_requests`.`created_at`) = 6 THEN 1
+        ELSE 0
+    END) - (CASE
+        WHEN WEEKDAY(`sp_requests`.`completado_at`) = 5 THEN 1
+        ELSE 0
+    END)))
+            AND(DATEDIFF(NOW(), `sp_requests`.`created_at`) - (WEEK(NOW()) - WEEK(`sp_requests`.`created_at`)) * 2 - (CASE
+        WHEN WEEKDAY(`sp_requests`.`created_at`) = 6 THEN 1
+        ELSE 0
+    END) - (CASE
+        WHEN WEEKDAY(NOW()) = 5 THEN 1
+        ELSE 0
+    END) > 5)
+    AND (`sp_requests`.`active` = 1)
+        ) 
     ");
     }
 
